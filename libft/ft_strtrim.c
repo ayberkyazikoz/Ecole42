e28a2943219ce1388c91e0dbf5b3564e96213371
ayberkyazikoz <ayberkyazikoz@gmail.com>
2023-07-16 21:21:42 +0300

    strtrim has been updated.

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,36 +12,22 @@
 
 #include "libft.h"
 
-int	check(char a, char const *set)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (a == set[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
-	size_t	i;
-	char	*rtn;
+	char	*str;
+	int		i;
+	int		j;
 
-	i = 0;
 	if (!s1)
 		return (NULL);
-	len = ft_strlen(s1) - 1;
-	while (check(s1[i], set))
+	if (!set)
+		return (ft_strdup(s1));
+	i = 0;
+	j = (ft_strlen(s1) - 1);
+	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	while (check(s1[len], set))
-		len--;
-	rtn = ft_substr(s1, i, len - i + 1);
-	if (!rtn)
-		return (NULL);
-	return (rtn);
+	while (s1[i] && ft_strchr(set, s1[j]))
+		j--;
+	str = ft_substr(s1, i, ((j - i) + 1));
+	return (str);
 }
