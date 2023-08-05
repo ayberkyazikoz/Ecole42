f8bde2f96b7cf6c882e4c146d32fc8feeefe2f9e
ayberkyazikoz <ayberkyazikoz@gmail.com>
2023-08-05 19:29:46 +0300

    folder updated. It may has tab errors!

---
+++ ft_printf/ft_words.c
@@ -0,0 +1,22 @@
+#include "ft_printf.h"
+
+void	ft_putcharacter_length(char character, int *length)
+{
+	write(1, &character, 1);
+	(*length)++;
+}
+
+void	ft_string(char *args, int *length)
+{
+	size_t	i;
+
+	i = 0;
+	if (!args)
+	{
+		write(1, "(null)", 6);
+		(*length) += 6;
+		return ;
+	}
+	while (args[i++] != '\0')
+		ft_putcharacter_length(args[i], length);
+}
